b4cddd9ef32d99aa670e3559d88de3e7aa636e66
Vesa Luukkala <vesa.luukkala@gmail.com>
2013-02-19 14:48:37 +0200

    Copyright year

--- testaspparse.hs
+++ testaspparse.hs
@@ -10,7 +10,7 @@
 -- ghci> runTestTT tests
 -- 
 -- 
--- Copyright 2012 Vesa Luukkala
+-- Copyright 2012,2013 Vesa Luukkala
 -- 
 -- Permission is hereby granted, free of charge, to any person obtaining
 -- a copy of this software and associated documentation files (the
